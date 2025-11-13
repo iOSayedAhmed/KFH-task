8912026830150b40cf4ca6aded1e22205cad51c9
eahmed <60572628+iOSayedAhmed@users.noreply.github.com>
2025-11-13 10:36:52 +0200

    Initial Commit

---
+++ KFH-task/KFH_taskApp.swift
@@ -0,0 +1,20 @@
+//
+//  KFH_taskApp.swift
+//  KFH-task
+//
+//  Created by iOSAYed on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct KFH_taskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
